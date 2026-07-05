9918a8f1075d857069cc45e86e47964e3ca9fbc6
paradisesweethouseinfo <paradisesweethouseinfo@gmail.com>
2026-07-06 04:29:30 +0530

    update website

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,19 @@
-import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import Navbar from "../components/Navbar";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      alert("Login successful");
-    } catch (err) {
-      alert(err.message);
-    }
-  };
-
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="p-6 bg-white shadow rounded-xl w-80">
-        <h2 className="text-xl font-bold mb-4">Student Login</h2>
+    <>
+      <Navbar />
 
-        <input
-          className="w-full border p-2 mb-2"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <div className="container pt-36 pb-24">
 
-        <input
-          className="w-full border p-2 mb-4"
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <h1 className="text-5xl font-bold">
 
-        <button
-          onClick={handleLogin}
-          className="w-full bg-green-600 text-white py-2 rounded"
-        >
-          Login
-        </button>
+          Student Login
+
+        </h1>
+
       </div>
-    </div>
+    </>
   );
 }
