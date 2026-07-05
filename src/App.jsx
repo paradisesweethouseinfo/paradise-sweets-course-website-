43e9a99e4d7ba3f90c29a8025ec218b232d135f0
paradisesweethouseinfo <paradisesweethouseinfo@gmail.com>
2026-07-05 23:45:28 +0530

    update website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>HOME PAGE</h1>} />
-        <Route path="/courses" element={<h1>COURSES PAGE</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
